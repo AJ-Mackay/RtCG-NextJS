9a224d34b9b92123b1449ccf18996902da6f0312
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-05 16:57:06 +0000

    Linking Between Pages

--- nextjs-demo/pages/news/index.js
+++ nextjs-demo/pages/news/index.js
@@ -1,8 +1,23 @@
 // our-domain.com/news
 // pages/news.js === pages/news/index.js *ONLY FOR NON-NESTED FILES!!!*
 
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
