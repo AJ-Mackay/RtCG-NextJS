fad0f141584a3ac798dd54c9f6d45041b95502d9
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-08 14:00:44 +0000

    Getting Meetup Details Data & Preparing Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,33 +1,37 @@
 // our-domain.com/<any variable>
+import { MongoClient, ObjectId } from 'mongodb';
+
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://cdn.cloudflare.steamstatic.com/steam/apps/930500/capsule_616x353.jpg"
-      title="A New Meetup"
-      address="Uncharted Backwaters, Unfashionable End, Western Spiral Arm, H2G2"
-      description="This is a new meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 // ** REQUIRED WHEN USING getStaticProps **
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    `mongodb+srv://ash:${process.env.REACT_APP_DBPASSWORD}@cluster0.ehnoo.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-      },
-      {
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
@@ -36,18 +40,27 @@ export async function getStaticProps(context) {
 
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  const client = await MongoClient.connect(
+    `mongodb+srv://ash:${process.env.REACT_APP_DBPASSWORD}@cluster0.ehnoo.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          'https://cdn.cloudflare.steamstatic.com/steam/apps/930500/capsule_616x353.jpg',
-        id: meetupId,
-        title: 'First Meetup',
-        address:
-          'Uncharted Backwaters, Unfashionable End, Western Spiral Arm, H2G2',
-        description: 'This is a new meetup',
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
