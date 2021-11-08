36c876a3e09237a9b06d8ab2d5701832a5e1ba7e
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-08 10:35:42 +0000

    Outputting a List of Meetups

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
 // our-domain.com/
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/%CE%A3%CE%B1%CE%B3%CF%81%CE%AC%CE%B4%CE%B1_%CE%A6%CE%B1%CE%BC%CE%AF%CE%BB%CE%B9%CE%B1_2941.jpg/1386px-%CE%A3%CE%B1%CE%B3%CF%81%CE%AC%CE%B4%CE%B1_%CE%A6%CE%B1%CE%BC%CE%AF%CE%BB%CE%B9%CE%B1_2941.jpg',
+    address: 'Carrer de Mallorca, Barcelona',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/41/BlogHer_08_-_Sesame_Street_Suite_%282682321763%29.jpg',
+    address: '123 Sesame Street, NYC',
+    description: 'This is a second meetup!',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
