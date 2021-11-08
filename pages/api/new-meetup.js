bd1fda6ef830202ce2b0d2446dbbdf78979f4c87
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-08 13:20:45 +0000

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,25 @@
 // our-domain.com/api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      `mongodb+srv://ash:${process.env.REACT_APP_DBPASSWORD}@cluster0.ehnoo.mongodb.net/meetups?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
