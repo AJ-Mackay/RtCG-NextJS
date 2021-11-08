0a8c7ba595d8f41215e4a0edb0eb9d164142e07a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-08 12:16:15 +0000

    Preparing Paths with "getStaticPaths" & Working With Fallback Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,6 +12,25 @@ function MeetupDetails() {
   );
 }
 
+// ** REQUIRED WHEN USING getStaticProps **
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
   // fetch data for a single meetup
 
