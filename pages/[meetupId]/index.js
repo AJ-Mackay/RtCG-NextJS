185a0d31580aadf81ca6edc9a4678cf8edd51c19
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-08 12:04:54 +0000

    Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,26 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://cdn.cloudflare.steamstatic.com/steam/apps/930500/capsule_616x353.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address:
+          'Uncharted Backwaters, Unfashionable End, Western Spiral Arm, H2G2',
+        description: 'This is a new meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
