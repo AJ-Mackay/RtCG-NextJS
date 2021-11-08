9377eaf8334cd88dae500e0809cdfc4698c14c8f
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-08 11:15:51 +0000

    Adding Custom Components & CSS Modules

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,15 @@
 // our-domain.com/<any variable>
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://cdn.cloudflare.steamstatic.com/steam/apps/930500/capsule_616x353.jpg"
+      title="A New Meetup"
+      address="Uncharted Backwaters, Unfashionable End, Western Spiral Arm, H2G2"
+      description="This is a new meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
